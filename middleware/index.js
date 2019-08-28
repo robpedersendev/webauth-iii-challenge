0659c67e5cf10e8ae3db6bfcc153cdc8736b5acf
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-08-27 22:17:19 -0600

    Added the middleware folder

---
+++ middleware/index.js
@@ -0,0 +1,25 @@
+const bcrypt = require("bcryptjs");
+
+const jwt = require("jsonwebtoken");
+
+const Users = require("../db-model/index");
+const secrets = require("../config/secrets");
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        //Bad Token!
+        res.status(401).json({ message: "What happened" });
+      } else {
+        //The token is a good token!
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "No Token Bruh" });
+  }
+};
