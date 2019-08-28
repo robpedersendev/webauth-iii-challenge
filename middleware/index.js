6251d24ba1df1a8357a21394d215c31ae0c32820
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-08-27 22:47:52 -0600

    Added console.log method to the middleware

--- middleware/index.js
+++ middleware/index.js
@@ -7,7 +7,7 @@ const secrets = require("../config/secrets");
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
-
+  console.log(token);
   if (token) {
     jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
       if (err) {
